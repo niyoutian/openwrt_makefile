66f00230921edef12bfb68c072e08d970468e01b
niyoutian <niyoutian_0510@163.com>
2021-03-30 09:30:57 +0800

    2021-03-30

--- openwrt_cc_v3.0/build.pl
+++ openwrt_cc_v3.0/build.pl
@@ -7,8 +7,7 @@ use strict;
 use Getopt::Long;
 
 
-my $root_dir = getcwd;
-my $confs_dir = "$root_dir/confs"; 
+my $root_dir = getcwd; 
 
 my $P = $0;
 $P =~ s@.*/@@g;
@@ -27,6 +26,7 @@ sub get_all_conffiles {
 #------------------------------------------------------------------------- 
 
 
+
 sub help {
         my ($exitcode) = @_;
 
@@ -35,13 +35,16 @@ Usage: $P [target]...
 Version: $V
 
 Options:
-  --target testapp(,extcc3.0...)   target various comma separated types
+  --target testapp(,extcc...)   target various comma separated types
   -c, --continue             continue build
   -h, --help, --version      display this help and exit
 
 for example:
-  $P --target testapp
-  $P --target testapp,extcc3.0,u01,inter_connd
+  ./$P --target testapp
+  ./$P --target u01
+  ./$P --target extcc
+  ./$P --target opmaintain
+  $P --target testapp,extcc,u01,inter_connd,opmaintain
 EOM
 
         exit($exitcode);
@@ -71,48 +74,118 @@ foreach my $word (@target) {
         next if ($word =~ m/testapp/);
         next if ($word =~ m/u01/);
 	next if ($word =~ m/inter_connd/);
+	next if ($word =~ m/opmaintain/);
 	printf("$word is not the right target!\n");
         exit(1);
 }
 
-my @confs_files=get_all_conffiles($confs_dir);
-for my $filename (@confs_files) {
-    next if ($filename eq '.' or $filename eq '..');
-    next if (!($filename =~ m/^.+\.config$/));
-    next if ($filename eq 'hi5663.config');
-#    $count++;
-#    next if ($count < 25);
-    next if ($filename eq 'zx279127.config');
-    next if ($filename eq 'zx279128.config');
-    next if ($filename eq 'sd5117x.config');
-    next if ($filename eq 'zx279131.config');
-    printf("compile $filename ...\n");
-    system("rm -f .config") == 0 or die "[$filename] rm -f .config failed:$?";
-    system("ln -sf confs/$filename .config") == 0 or die "[$filename] ln -sf confs/$filename .config failed:$?";
-    foreach my $word (@target) {
-	if ($continue eq 1) {
-            my $temp = $filename =~ s/^(.*).config$/$1/r;
-            my $tmp_dir = "$root_dir/bin/$temp/packages/base";
-            my @file_list = glob "$tmp_dir/$word*$temp.ipk";
-            next if (@file_list);
-	}
-        printf("make defconfig $filename $word\n");
-        system("make defconfig") == 0 or die "[$filename:$word] make defconfig failed:$?";
-        if ($word eq "testapp") {
-            printf("$filename:$word\n");
-            system("sed -i 's/# CONFIG_PACKAGE_testappv3 is not set/CONFIG_PACKAGE_testappv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
-        } elsif ($word eq "extcc3.0") {
-            printf("$filename:$word\n");
-            system("sed -i 's/# CONFIG_PACKAGE_extccv3 is not set/CONFIG_PACKAGE_extccv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";		
-        } elsif ($word eq "u01") {
-            printf("$filename:$word\n");
-            system("sed -i 's/# CONFIG_PACKAGE_u01v3 is not set/CONFIG_PACKAGE_u01v3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
-        } elsif ($word eq "inter_connd") {
-            printf("$filename:$word\n");
-            system("sed -i 's/# CONFIG_PACKAGE_inter_conndv3 is not set/CONFIG_PACKAGE_inter_conndv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
-        }
-        system("make prepare") == 0 or die "[$filename:$word] make prepare failed:$?";
-        system("make package/upointech/$word/clean") == 0 or die "[$filename:$word] make package/upointech/$word/clean failed:$?";
-        system("make package/upointech/$word/compile") == 0 or die "[$filename:$word] make package/upointech/$word/compile failed:$?";;
+sub build_sdk3 {
+    my $confs_dir = "$root_dir/confs";
+    my @confs_files=get_all_conffiles($confs_dir);
+    for my $filename (@confs_files) {
+        next if ($filename eq '.' or $filename eq '..');
+        next if (!($filename =~ m/^.+\.config$/));
+        next if ($filename eq 'hi5663.config');
+        #    $count++;
+        #    next if ($count < 25);
+        next if ($filename eq 'zx279127.config');
+        next if ($filename eq 'zx279128.config');
+        next if ($filename eq 'sd5117x.config');
+        next if ($filename eq 'zx279131.config');
+        printf("compile $filename ...\n");
+        system("rm -f .config") == 0 or die "[$filename] rm -f .config failed:$?";
+        system("ln -sf confs/$filename .config") == 0 or die "[$filename] ln -sf confs/$filename .config failed:$?";
+        foreach my $word (@target) {
+            if ($continue eq 1) {
+                my $temp = $filename =~ s/^(.*).config$/$1/r;
+                my $tmp_dir = "$root_dir/bin/$temp/packages/base";
+                my @file_list = glob "$tmp_dir/$word*$temp.ipk";
+                next if (@file_list);
+            }
+            printf("make defconfig $filename $word\n");
+            system("make defconfig") == 0 or die "[$filename:$word] make defconfig failed:$?";
+			if ($word eq "testapp") {
+				printf("$filename:$word\n");
+				system("sed -i 's/# CONFIG_PACKAGE_testappv3 is not set/CONFIG_PACKAGE_testappv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
+			} elsif ($word eq "extccv3") {
+				printf("$filename:$word\n");
+				system("sed -i 's/# CONFIG_PACKAGE_extccv3 is not set/CONFIG_PACKAGE_extccv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";		
+			} elsif ($word eq "u01") {
+				printf("$filename:$word\n");
+				system("sed -i 's/# CONFIG_PACKAGE_u01v3 is not set/CONFIG_PACKAGE_u01v3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
+			} elsif ($word eq "inter_connd") {
+				printf("$filename:$word\n");
+				system("sed -i 's/# CONFIG_PACKAGE_inter_conndv3 is not set/CONFIG_PACKAGE_inter_conndv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
+			} elsif ($word eq "opmaintain") {
+			    printf("$filename:$word\n");
+			    system("sed -i 's/# CONFIG_PACKAGE_opmaintainv3 is not set/CONFIG_PACKAGE_opmaintainv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
+		    }
+
+			system("make prepare") == 0 or die "[$filename:$word] make prepare failed:$?";
+			system("make package/upointech/$word/clean") == 0 or die "[$filename:$word] make package/upointech/$word/clean failed:$?";
+			if ($word eq "opmaintain") {
+				system("make package/network/utils/curl/clean") == 0 or die "[$filename:$word] make  package/network/utils/curl/clean failed:$?";
+				system("make package/network/utils/curl/compile") == 0 or die "[$filename:$word] make  package/network/utils/curl/compile failed:$?";
+			}
+			system("make package/upointech/$word/compile") == 0 or die "[$filename:$word] make package/upointech/$word/compile failed:$?";
+		}
     }
 }
+
+sub build_sdk2 {
+    my $confs_dir = "$root_dir";
+    print "build_sdk2 \n";
+    print "confs_dir=$confs_dir\n";
+    my @confs_files=get_all_conffiles($confs_dir);
+    for my $filename (@confs_files) {
+        next if (!($filename =~ m/^.+\.config$/));
+        next if ($filename eq 'zte-bcm6858.config');
+        printf("compile $filename ...\n");
+        system("rm -f .config") == 0 or die "[$filename] rm -f .config failed:$?";
+        system("ln -sf $filename .config") == 0 or die "[$filename] ln -sf $filename .config failed:$?";
+        foreach my $word (@target) {
+            if ($continue eq 1) {
+                my $temp = $filename =~ s/^.*-(.*).config$/$1/r;
+                print "temp=$temp\n";
+                $temp = "sd5116" if ($temp eq "5116");
+                $temp = "sd5116v1" if ($temp eq "5116v1");
+                $temp = "zx279100" if ($temp eq "279100");
+                $temp = "zx279127" if ($temp eq "279127");
+                $temp = "zx279128" if ($temp eq "279128");
+                print "temp2=$temp\n";
+                my $tmp_dir = "$root_dir/bin/$temp/packages/base";
+                my @file_list = glob "$tmp_dir/$word*$temp.ipk";
+                next if (@file_list);
+            }
+            printf("make defconfig $filename $word\n");
+            system("make defconfig") == 0 or die "[$filename:$word] make defconfig failed:$?";
+			if ($word eq "testapp") {
+				printf("$filename:$word\n");
+				system("sed -i 's/# CONFIG_PACKAGE_testappv3 is not set/CONFIG_PACKAGE_testappv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
+			} elsif ($word eq "extcc") {
+				printf("$filename:$word\n");
+				system("sed -i 's/# CONFIG_PACKAGE_extcc is not set/CONFIG_PACKAGE_extcc=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";		
+			} elsif ($word eq "u01") {
+				printf("$filename:$word\n");
+				system("sed -i 's/# CONFIG_PACKAGE_u01 is not set/CONFIG_PACKAGE_u01=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
+			} elsif ($word eq "inter_connd") {
+				printf("$filename:$word\n");
+				system("sed -i 's/# CONFIG_PACKAGE_inter_conndv3 is not set/CONFIG_PACKAGE_inter_conndv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
+			} elsif ($word eq "opmaintain") {
+                printf("$filename:$word\n");
+                system("sed -i 's/# CONFIG_PACKAGE_opmaintainv3 is not set/CONFIG_PACKAGE_opmaintainv3=y/g' .config") == 0 or die "[$filename:$word] modify .config failed:$?";
+            }
+			system("make prepare") == 0 or die "[$filename:$word] make prepare failed:$?";
+			system("make package/upointech/$word/clean") == 0 or die "[$filename:$word] make package/upointech/$word/clean failed:$?";
+            system("make package/upointech/$word/compile") == 0 or die "[$filename:$word] make package/upointech/$word/compile failed:$?";
+        }
+    }
+}
+
+my $sdk_ver = 0;
+$sdk_ver = 2 if ($root_dir =~ /openwrt_cc/);
+$sdk_ver = 3 if ($root_dir =~ /openwrt_cc_v3.0/);
+print "sdk_ver = $sdk_ver\n";
+build_sdk2() if ($sdk_ver == 2);
+build_sdk3() if ($sdk_ver == 3);
+exit(0);
